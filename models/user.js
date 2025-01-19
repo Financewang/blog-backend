160307114e88eeb0c1c7ec07e43827e0eb86308a
FinanceWang <chuanxiao579@gmail.com>
2025-01-19 21:21:44 +0800

    Switch from bcrypt to bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs'); // 改为使用 bcryptjs
 
 // 定义用户模型的 Schema
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true }, // 用户名，必须唯一
+  username: { type: String, required: true, unique: true }, // 用户名，必填
   password: { type: String, required: true } // 密码
-}, { timestamps: true }); // 自动生成创建和更新时间字段
+}, { timestamps: true }); // 自动生成创建和更新的时间字段
 
 // 密码加密中间件
 UserSchema.pre('save', async function (next) {
@@ -16,9 +16,9 @@ UserSchema.pre('save', async function (next) {
 
   try {
     // 生成盐值
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcryptjs.genSalt(10);
     // 使用盐值对密码进行加密
-    const hashedPassword = await bcrypt.hash(this.password, salt);
+    const hashedPassword = await bcryptjs.hash(this.password, salt);
     // 保存加密后的密码
     this.password = hashedPassword;
     next();
