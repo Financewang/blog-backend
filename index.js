5748f153d61a698ee6f03d5a46fe17e14f8bd3c9
FinanceWang <chuanxiao579@gmail.com>
2025-01-19 22:01:15 +0800

    Fix dependencies and MongoDB connection

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, { // 使用 .env 中的 MONGO_URI
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  retryWrites: true,
-  w: 'majority'
 })
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Failed to connect to MongoDB Atlas', err));
